8f6326b945ffd7d9a427a5e99320fe6a5554e327
almie0203 <=>
2021-01-25 14:57:29 +0800

    change DB from sqlite to mongodb

--- src/app.py
+++ src/app.py
@@ -1,82 +1,120 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_cors import CORS
-import os
+from flask import Flask, request,jsonify
+from flask_cors import CORS 
+import pymongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson.json_util import dumps
+import json
+from bson.objectid import ObjectId
 
-app = Flask(__name__)
-CORS(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app = Flask(__name__) 
+conn = pymongo.MongoClient("mongodb+srv://almie0203:!L0veyou143@cluster0.vh54v.mongodb.net/flaskmongorestful?retryWrites=true&w=majority",connect=False)
+  
+db = conn.flaskmongorestful
 
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(70), unique=True)
-    description = db.Column(db.String(200))
-
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-
-db.create_all()
-
-class TaskSchema(ma.Schema):
-    class Meta:
-        fields = ('id','title','description')
-
-task_schema = TaskSchema()
-tasks_schema = TaskSchema(many=True)
-
-@app.route('/tasks',methods=['POST'])
-def create_task():
-
-    title = request.json['title']
-    description = request.json['description']
-
-    new_task = Task(title,description)
-    db.session.add(new_task)
-    db.session.commit()
-    return task_schema.jsonify(new_task)
-
-@app.route('/tasks',methods=['GET'])
-def get_tasks():
-    all_tasks = Task.query.all()
-    result = tasks_schema.dump(all_tasks)
-    return jsonify(result)
-
-@app.route('/tasks/<id>',methods=['GET'])
-def get_task(id):
-    task = Task.query.get(id)
-    return task_schema.jsonify(task)
-
-@app.route('/tasks/<id>',methods=['PUT'])
-def update_task(id):
-    task = Task.query.get(id)
-
-    title = request.json['title']
-    description = request.json['description']
-
-    task.title = title
-    task.description = description
-
-    db.session.commit()
-    return task_schema.jsonify(task)
-
-@app.route('/tasks/<id>',methods=['DELETE'])
-def delete_task(id):
-    task = Task.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return task_schema.jsonify(task)
-
-@app.route('/',methods=['GET'])
-def index():
-    return jsonify({"message":"Welcome to my API"})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/users', methods=['POST', 'GET'])
+def get_all_users():
     
+    # POST a data to database
+    if request.method == 'POST':
+        body = request.json
+        name = body['name']
+        birthdate = body['birthdate']
+        email = body['email']
+        password = generate_password_hash(body['password'])
+
+        # db.users.insert_one({
+        db['users'].insert_one({
+            "name": name,
+            "birthdate": birthdate,
+            "email": email,
+            "password": password,
+        })
+        return jsonify({
+            'status': 'Data is posted to MongoDB!',
+            "name": name,
+            "birthdate": birthdate,
+            "email": email,
+            #"password": password,
+        })
+    
+    # GET all data from database
+    if request.method == 'GET':
+        allData = db['users'].find()
+        dataJson = []
+        for data in allData:
+            id = data['_id']
+            name = data['name']
+            birthdate = data['birthdate']
+            email = data['email']
+            #password = data['password']
+
+            dataDict = {
+                "id":str(id),
+                "name": name,
+                "birthdate": birthdate,
+                "email": email,
+                #"password": password,
+            }
+            dataJson.append(dataDict)
+        print(dataJson)
+        return jsonify(dataJson)
+
+@app.route('/users/<string:id>', methods=['GET', 'DELETE', 'PUT'])
+def get_by_id(id):
+
+    # GET a specific data by id
+    if request.method == 'GET':
+        data = db['users'].find_one({'_id': ObjectId(id)})
+        id = data['_id']
+        name = data['name']
+        birthdate = data['birthdate']
+        email = data['email']
+        dataDict = {
+            "id":str(id),
+            "name": name,
+            "birthdate": birthdate,
+            "email": email,
+        }
+        print(dataDict)
+        return jsonify(dataDict)
+        
+    # DELETE a data
+    if request.method == 'DELETE':
+        db['users'].delete_many({'_id': ObjectId(id)})
+        print('\n # Deletion successful # \n')
+        return jsonify({'status': 'Data id: ' + id + ' is deleted!'})
+
+    # UPDATE a data by id
+    if request.method == 'PUT':
+        body = request.json
+        first = body['name']['first']
+        middle = body['name']['middle']
+        last = body['name']['last']
+        birthdate = body['birthdate']
+        email = body['email']
+
+        db['users'].update_one(
+            {'_id': ObjectId(id)},
+            {
+                "$set": {
+                    "name":{
+                        "first":first,
+                        "middle":middle,
+                        "last":last
+                    },
+                    "birthdate":birthdate,
+                    "email":email
+                }
+            }
+        )
+
+        print('\n # Update successful # \n')
+        return jsonify({'status': 'Data id: ' + id + ' is updated!'})
+
+if __name__ == "__main__":
+    app.run(
+        debug=True, 
+        #port=5001, 
+        #host='0.0.0.0'
+    )
