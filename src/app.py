bc2cd7232dcde5d1034e2e135ca66a76d19f11fc
almie0203 <=>
2021-01-25 15:05:32 +0800

    change to hosted

--- src/app.py
+++ src/app.py
@@ -115,6 +115,6 @@ def get_by_id(id):
 if __name__ == "__main__":
     app.run(
         debug=True, 
-        #port=5001, 
-        #host='0.0.0.0'
+        port=5001, 
+        host='0.0.0.0'
     )
