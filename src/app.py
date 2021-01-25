379794b4e27320141a83ee14d3bb22ffbd09b489
almie0203 <=>
2021-01-25 15:37:21 +0800

    dfds

--- src/app.py
+++ src/app.py
@@ -114,7 +114,7 @@ def get_by_id(id):
 
 if __name__ == "__main__":
     app.run(
-        debug=True, 
-        port=5001, 
-        host='0.0.0.0'
+        debug=True
+        #port=5001, 
+        #host='0.0.0.0'
     )
